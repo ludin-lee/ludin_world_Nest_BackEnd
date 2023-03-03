d4ba8e2db1b63a4778a5c7cab36729a2dcef8c64
ludin-lee <alsdyd0331@naver.com>
2023-03-03 16:45:33 +0900

    :sparkles: CRUD 컨트롤러 틀  구현

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,18 +1,29 @@
-import { Controller,Get,Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
 
-    @Get()
-    getAll(){
-        return 'this will return all movies'
-    }
-    @Get(':id')
-    getOne(@Param('id') id:string){
-        return `One movie ${id}`
-    }
-    @Post()
-    create(){
-        return 'zzzz'
-    }
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
+  }
 }
